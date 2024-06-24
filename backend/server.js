4ae523edc92f0bc989ee82d1bbf0d66d84a301f0
Aamirkhan121 <aamir73690@gmail.com>
2024-06-24 12:23:17 +0530

    5th part

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path"
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -8,6 +9,8 @@ import connectTOMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
 const PORT=process.env.PORT||5000;
+
+const __dirname=path.resolve()
 dotenv.config();
 
 app.use(express.json()); // to parse  the incoming requests with JSON payload (from req.body)
@@ -17,9 +20,12 @@ app.use("/api/auth",authRouter)
 app.use("/api/messages",messageRoutes)
 app.use("/api/users",userRoutes)
 
-// app.use("/",(req,res)=>{
-//     res.status(200).send("Hello Aamir!")
-// })
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
 
 server.listen(PORT,()=>{
     connectTOMongoDB()
